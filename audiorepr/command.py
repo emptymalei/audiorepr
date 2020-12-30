1f8ad8e4a8283577bf9942e5074647ee49198c9b
LM <hi@leima.is>
2020-12-30 16:53:21 +0100

    added command line tool

---
+++ audiorepr/command.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import pandas as pd
+
+import click
+from loguru import logger
+from audiorepr import audiolize
+
+# determine current working directory
+__CWD__ = os.getcwd()
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def audiorepr(ctx):
+    """
+    audiorepr is the root command line command
+    """
+    if ctx.invoked_subcommand is None:
+        click.echo("Hello {}".format(os.environ.get("USER", "")))
+        click.echo("Welcome to audiorepr.")
+    else:
+        click.echo("Loading Service: %s" % ctx.invoked_subcommand)
+
+@audiorepr.command()
+def help():
+    """
+    How to use command line tool of audiorepr
+    """
+
+    click.echo(
+        "The audiorepr command creates audio files out of a tabular data file.\n"
+        "`audiorepr help` shows this message.\n"
+        "`audiorepr create` command is the core command to create audio."
+    )
+
+@audiorepr.command()
+@click.option(
+    '-d', '--data',
+    type=click.Path(exists=True),
+    prompt=f"Please specify your data file\n"
+    f"Your current working directory is {__CWD__}\n"
+    f"Use absolute path or path relative to {__CWD__}.\n"
+)
+@click.option(
+    '-t', '--target',
+    type=click.Path(),
+    prompt=f"Please specify your output file\n"
+    f"Your current working directory is {__CWD__}\n"
+    f"Use absolute path or path relative to {__CWD__}.\n"
+)
+@click.option(
+    '-c', '--columns',
+    multiple=True, default=None
+)
+def create(data, target, columns):
+    """
+    creates audio from data file
+    """
+
+    click.echo(
+        f"Using {data} as data source."
+    )
+
+    df = pd.read_csv(data)
+    click.echo(
+        f"Data loaded"
+    )
+    audiolize.audiolizer(df, target=target, pitch_columns=columns)
+    click.echo(
+        f"Audio file has been saved as {target}"
+    )
+
+
+
+if __name__ == "__main__":
+
+    pass
